d34e5de6bf7c3d7edb54294941a8b1f635671769
Samuel Williams <samuelhwilliams@gmail.com>
2022-10-17 09:04:09 +0100

    Update comments

--- src/main/java/uk/gov/service/notify/RetentionPeriodDuration.java
+++ src/main/java/uk/gov/service/notify/RetentionPeriodDuration.java
@@ -11,11 +11,13 @@ public class RetentionPeriodDuration {
     private final ChronoUnit unit;
 
     /***
-     * Creates an employee with the specified name and
-     * @param amount - The amount of units of time. Example: If unit is ChronoUnit.WEEKS, and amount is 52,
+     * Create a RetentionPeriod instance for a specific period of time.
+     *
+     * @param amount - The amount of units of time. Example: If unit is
+     *               ChronoUnit.WEEKS, and amount is 52,
      *               then the duration is 52 weeks.
-     * @param unit - The unit of time that is being represented as a ChronoUnit. Currently only supports
-     *             ChronoUnit.WEEKS until API changes allow for other unit types.
+     * @param unit   - The unit of time that is being represented as a ChronoUnit.
+     *               Currently only supports ChronoUnit.WEEKS.
      */
     public RetentionPeriodDuration(int amount, ChronoUnit unit) {
         this.amount = amount;
@@ -31,16 +33,17 @@ public class RetentionPeriodDuration {
     }
 
     /***
-     * The toString method converts the object into a string suitable for processing by the Notifications API
-     * @return A string, representing a retention period duration for files, that complies with the API's expectations
+     * The toString method converts the object into a string suitable for processing
+     * by the Notifications API
+     *
+     * @return A string, representing a retention period duration for files, that
+     *         complies with the API's expectations
      */
     @Override
-    public String toString(){
-        // The unit has to be lower cased for the API to accept it
+    public String toString() {
         return String.format(
                 "%d %s",
                 getAmount(),
-                getUnit().toString().toLowerCase(Locale.ROOT)
-        );
+                getUnit().toString().toLowerCase(Locale.ROOT));
     }
 }
