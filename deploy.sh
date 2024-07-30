df330284a71e5c69a4bd02e9db6f5afb67b3487a
joybytes <171517790+joybytes@users.noreply.github.com>
2024-07-30 14:50:23 +0100

    chore: add maven token details

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,19 @@ set -eo pipefail
 
 source environment.sh
 
-if [[ -z "${MAVEN_CENTRAL_PASSWORD}" ]]; then
+if [[ -z "${MAVEN_CENTRAL_TOKEN_USERNAME}" ]]; then
   # notify-pass is set up by our bash_profile/bashrc/etc files, so isnt available within this shell. have to do the
   # hard work manually
-  echo 'Maven password not set. Please run the following and then retry'
-  echo 'export MAVEN_CENTRAL_PASSWORD=$(notify-pass show credentials/maven-central/password)'
+  echo 'Maven token username not set. Please run the following and then retry'
+  echo 'export MAVEN_CENTRAL_TOKEN_USERNAME=$(notify-pass show credentials/maven-central/token-username)'
+  exit 1
+fi
+
+if [[ -z "${MAVEN_CENTRAL_TOKEN_PASSWORD}" ]]; then
+  # notify-pass is set up by our bash_profile/bashrc/etc files, so isnt available within this shell. have to do the
+  # hard work manually
+  echo 'Maven token password not set. Please run the following and then retry'
+  echo 'export MAVEN_CENTRAL_TOKEN_PASSWORD=$(notify-pass show credentials/maven-central/token-password)'
   exit 1
 fi
 
